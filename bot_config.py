a430f965ba6126344b65158cd09e264e4eee7603
Blacktenix <vecheslavik2008@gmail.com>
2025-03-14 19:47:06 +0300

    Add bot_config.py

---
+++ bot_config.py
@@ -0,0 +1,35 @@
+HELLO_LIST = [
+    "hello",
+    "hi",
+    "hey",
+    "howdy",
+    "greetings",
+    "salutations",
+    "yo",
+    "sup",
+    "what's up",
+    "good day",
+    "ahoy",
+    "hola",
+    "ciao",
+    "bonjour",
+    "aloha",
+    "namaste",
+    "shalom",
+]
+INFO_LIST = [
+    "info",
+    "information",
+    "data",
+    "facts",
+    "insights",
+    "knowledge",
+    "summary",
+    "overview",
+]
+
+GIF_LIST = [
+    "https://i.giphy.com/BVSMbtX5ZRGqwnCQnX.webp",
+    "https://i.giphy.com/VIKa3CjZDCoymNcBY5.webp",
+    "https://i.giphy.com/yoJC2GnSClbPOkV0eA.webp"
+]
